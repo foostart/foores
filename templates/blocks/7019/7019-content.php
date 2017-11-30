c363bd3271e8d80843e6f103813e57821ab91af8
Thy96 <33407163+Thy96@users.noreply.github.com>
2017-11-30 12:33:56 +0700

    up 7006 & 7019
    up 7006 & 7019

--- templates/blocks/7019/7019-content.php
+++ templates/blocks/7019/7019-content.php
@@ -23,6 +23,7 @@
                         <h5>Today Specials</h5>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-4 m3-content-menu">
+                        <!--Breakfast - 1 -->
                         <div class="m3-content-menu-food">
                             <h2>Breakfast</h2>
                         </div>
@@ -39,6 +40,25 @@
                                 <span class="price">$11 each</span></h3>
                                 <div class="m3-content-menu-food-description">Hot coffee in the company of Beautiful homemade croissants, each containing a bar of high-quality dark chocolate, make for ...</div>
                         </div>
+
+                        <!--Breakfast - 2 -->
+                        <div class="m3-content-menu-food">
+                            <h2>Breakfast</h2>
+                        </div>
+                        <div class="m3-content-menu-food">
+                            <h3>Cashew Chicken With Stir-Fry Vegetables &amp; Green Salad<span class="price">$22</span></h3>
+                            <div class="m3-content-menu-food-description">Cashew Chicken With Stir-Fry Vegetables served with green salad, sprouts and stewed mushrooms.</div>
+                        </div>
+                        <div class="m3-content-menu-food">
+                            <h3>Optic Big Breakfast Combo Menu<span class="price">$20</span></h3>
+                            <div class="m3-content-menu-food-description"></div>
+                        </div>
+                        <div class="m3-content-menu-food">
+                            <h3>Coffee and Pain au chocolat (Chocolate croissant) Combo Menu
+                                <span class="price">$11 each</span></h3>
+                                <div class="m3-content-menu-food-description">Hot coffee in the company of Beautiful homemade croissants, each containing a bar of high-quality dark chocolate, make for ...</div>
+                        </div>
+
                     </div>
                           <!--Happy  hour-->
                     
