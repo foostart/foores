ecb5b4f9af7ae39baac65a0058f251eb0a8935d6
Vanthao481994 <vthao481994@gmail.com>
2017-11-29 20:47:53 -0800

    Comment
    Commen

--- templates/blocks/7007/7007-content.php
+++ templates/blocks/7007/7007-content.php
@@ -1,133 +1,102 @@
 <?php
-$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$url_host = $_SERVER['HTTP_HOST'];
+
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
 preg_match_all($pattern_uri, __DIR__, $matches);
+
 $url_path = $url_host . $matches[1][0];
+
 $url_path = str_replace('\\', '/', $url_path);
 ?>
-<!--show product-->
-<div class="modul-7004">
-    <div class="row m7007-tabs-all">
-        <div class="col-md-12">
+
+<div class="modul-7">
+    <div class="container">
+        <div class="row m7-tabs-all">
             <ul class="nav nav-tabs">
                 <li class="active"><a href="#all" data-toggle="tab">All<span>8</span></a></li>
                 <li><a href="#coffee" data-toggle="tab">Coffee Shop<span>4</span></a></li>
                 <li><a href="#gourmet" data-toggle="tab">Gourmet<span>3</span></a></li>
                 <li><a href="#recipes" data-toggle="tab">Recipes<span>4</span></a></li>
             </ul>
-        </div>
-
-        <!--tab content-->
-        <div class="tab-content">
-            <div class="tab-pane active" id="all">
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <div class="portfolio_details">
-                        <h3 class="title">
+            <div class="tab-content">
+                <div class="tab-pane active" id="all">
+                    <div class="col-lg-3 col-md-3 col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
                             <a>Jamie Oliver: Try Something New</a>
-                        </h3>
+                        </span>
                     </div>
-                    <img src="images/Meat-3-800x615.jpg" alt="reponsive-img">
-
-                </div>
-
-            </div>
-
-
-
-            <div class="tab-pane" id="coffee">
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <img src="images/Brunch-2-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Top 10 breakfast spots in Paris</a>
-                        </h3>
-                    </span>
-                </div>
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <img src="images/Coffee-3-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Caffe Latte Recipe</a>
-                        </h3>
-                    </span>
-                </div>
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <img src="images/tea-7-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Coffee for Your Health</a>
-                        </h3>
-                    </span>
-                </div>
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <img src="images/people-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Coffee with a friend is like capturing</a>
-                        </h3>
-                    </span>
-                </div>
-            </div>
-            <div class="tab-pane" id="gourmet">
-                <div class="col-md-3 col-sm-6  tab-content-pic">
-                    <img src="images/Seafood-2-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Creamy Leek & Potato Soup</a>
-                        </h3>
-                    </span>
-                </div>
-                <div class="col-md-3 col-sm-6  tab-content-pic">
-                    <img src="images/Brunch-2-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Top 10 breakfast spots in Paris</a>
-                        </h3>
-                    </span>
-                </div>
-                <div class="col-md-3 col-sm-6  tab-content-pic">
-                    <img src="images/Coffee-3-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Caffe Latte Recipe</a>
-                        </h3>
-                    </span>
-                </div>
-            </div>
-            <div class="tab-pane" id="recipes">
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <img src="images/Meat-3-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
                             <a>Jamie Oliver: Try Something New</a>
-                        </h3>
-                    </span>
+                        </span>
+                    </div>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
                 </div>
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <img src="images/Coffee-3-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Caffe Latte Recipe</a>
-                        </h3>
-                    </span>
+                <div class="tab-pane" id="coffee">
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/breakfast-8-300x233.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+
                 </div>
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <img src="images/sushi-1-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>7 Tips for the Best Sushi Party Ever</a>
-                        </h3>
-                    </span>
+                <div class="tab-pane" id="gourmet">
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/desserts-1-300x233.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+
                 </div>
-                <div class="col-md-3 col-sm-6 tab-content-pic">
-                    <img src="images/Sushi-2-800x615.jpg" alt="reponsive-img">
-                    <span>
-                        <h3>
-                            <a>Sushi Lovers or Beginners</a>
-                        </h3>
-                    </span>
+                <div class="tab-pane" id="recipes">
+                    <div class="col-lg-3 col-md-3 col-sm-6 tab-content-pic">
+                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
