b691369d8752c88badf2e0c4461f4ec9cab13b1e
Vanthao481994 <vthao481994@gmail.com>
2017-11-29 21:47:08 -0800

    Comment
    Comment

--- templates/blocks/7007/7007-content.php
+++ templates/blocks/7007/7007-content.php
@@ -15,58 +15,60 @@ $url_path = str_replace('\\', '/', $url_path);
 <div class="modul-7">
     <div class="container">
         <div class="row m7-tabs-all">
-            <ul class="nav nav-tabs">
-                <li class="active"><a href="#all" data-toggle="tab">All<span>8</span></a></li>
-                <li><a href="#coffee" data-toggle="tab">Coffee Shop<span>4</span></a></li>
-                <li><a href="#gourmet" data-toggle="tab">Gourmet<span>3</span></a></li>
-                <li><a href="#recipes" data-toggle="tab">Recipes<span>4</span></a></li>
-            </ul>
+            <div class="col-md-12">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#all" data-toggle="tab">All<span>8</span></a></li>
+                    <li><a href="#coffee" data-toggle="tab">Coffee Shop<span>4</span></a></li>
+                    <li><a href="#gourmet" data-toggle="tab">Gourmet<span>3</span></a></li>
+                    <li><a href="#recipes" data-toggle="tab">Recipes<span>4</span></a></li>
+                </ul>   
+            </div>
             <div class="tab-content">
                 <div class="tab-pane active" id="all">
                     <div class="col-lg-3 col-md-3 col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/1.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
                     </div>
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/2.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
                     </div>
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/3.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
                     </div>
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/4.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
                     </div>
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/5.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
                     </div>
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/6.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
                     </div>
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/7.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
                     </div>
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/8.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
@@ -74,16 +76,47 @@ $url_path = str_replace('\\', '/', $url_path);
                 </div>
                 <div class="tab-pane" id="coffee">
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/breakfast-8-300x233.jpg" alt="repinsove-img">
+                        <img src="images/3.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
                     </div>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/4.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                    <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/7.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/8.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                    
 
                 </div>
                 <div class="tab-pane" id="gourmet">
                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
-                        <img src="images/desserts-1-300x233.jpg" alt="repinsove-img">
+                        <img src="images/2.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/3.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                     <div class="col-lg-3 col-md-3  col-sm-6 tab-content-pic">
+                        <img src="images/4.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
@@ -92,7 +125,25 @@ $url_path = str_replace('\\', '/', $url_path);
                 </div>
                 <div class="tab-pane" id="recipes">
                     <div class="col-lg-3 col-md-3 col-sm-6 tab-content-pic">
-                        <img src="images/Meat-3-800x615.jpg" alt="repinsove-img">
+                        <img src="images/1.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                     <div class="col-lg-3 col-md-3 col-sm-6 tab-content-pic">
+                        <img src="images/4.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                     <div class="col-lg-3 col-md-3 col-sm-6 tab-content-pic">
+                        <img src="images/5.jpg" alt="repinsove-img">
+                        <span>
+                            <a>Jamie Oliver: Try Something New</a>
+                        </span>
+                    </div>
+                     <div class="col-lg-3 col-md-3 col-sm-6 tab-content-pic">
+                        <img src="images/7.jpg" alt="repinsove-img">
                         <span>
                             <a>Jamie Oliver: Try Something New</a>
                         </span>
