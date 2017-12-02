0abe85b9aeb46b744d6f10de979375e1484a7442
Vanthao481994 <vthao481994@gmail.com>
2017-12-02 10:30:34 -0800

    comment
    comment

--- templates/blocks/7007/7007.php
+++ templates/blocks/7007/7007.php
@@ -3,7 +3,6 @@
 $url_host = 'http://' . $_SERVER['HTTP_HOST'];
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-
 preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
@@ -22,9 +21,9 @@ $less->compileFile('less/7007.less', 'css/7007.css');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <script src="<?php echo $url_path ?>/js/jquery.min.js" type="text/javascript"></script>
-        <script src="<?php echo $url_path ?>/js/bootstrap.min.js" type="text/javascript"></script>
-        <link href="css/7007.css" rel="stylesheet" type="text/css"/>
+        <script src="<?php echo $url_path ?>/js/jquery.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/bootstrap.min.js"></script>
+        <link href="<?php echo $url_path ?>/css/7007.css" rel="stylesheet" type="text/css"/>
     </head> 
     <body> 
         <?php include '../7007/7007-content.php'; ?>
