5874e08ac98e5203aa2c2332f1f317fcdd512ba3
Vanthao481994 <vthao481994@gmail.com>
2017-12-04 14:32:16 -0800

    comment
    comment

--- templates/blocks/7002/7002-content.php
+++ templates/blocks/7002/7002-content.php
@@ -12,33 +12,33 @@ $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="container">
-<div class="type-7002">
-    <div class="m2-container">
-        <div class="m2-grip">
-            <div class="m2-grip-paralax">
-                <div class="m2-grip-background"></div>
-            </div>
-            <div class="m2-grip-content">
-                <div class="m2-grip-content-1 col-md-4 col-sm-4">
-                    <div class="m2-grip-content-1-left col-md-12">
-                        <h4>Tools</h4>
-                        <p >“Professional chefs rely on a battery of tools and equipment. Chefs must be conscious of their kitchen tools when planning a menu and understand their use and production capacity.”</p>
+    <div class="m2">
+        <div class="m2-container">
+            <div class="m2-grip">
+                <div class="m2-grip-paralax">
+                    <div class="m2-grip-background"></div>
+                </div>
+                <div class="m2-grip-content">
+                    <div class="m2-grip-content-1 col-md-4 col-sm-4">
+                        <div class="m2-grip-content-1-left col-md-10">
+                            <h4>Tools</h4>
+                            <p>“Professional chefs rely on a battery of tools and equipment. Chefs must be conscious of their kitchen tools when planning a menu and understand their use and production capacity.”</p>
+                        </div>
                     </div>
-                </div>
-                <div class="m2-grip-content-2 col-md-4 col-sm-4">
-                    <img src="img/coffee1.png" alt="img-responsive" >
-                    <h3>Welcome</h3>
-                    <p>“First and foremost I am a chef, whether behind the stove at one of my Northern California restaurants or for the past 15 years in front of the camera on my Food Network cooking shows. Creating new dishes and flavor combinations that bring cooks and our restaurant guests pleasure is my job and I love it.”</p>
-                </div>
-                <div class="m2-grip-content-1 m2-grip-content-3 col-md-4 col-sm-4">
-                    <div class="m2-grip-content-1-left col-md-12">
-                        <h4>Food</h4>
-                        <p >“Creating new dishes and flavor combinations that bring cooks and our restaurant guests pleasure is my job and <br>I love it.”</p>
+                    <div class="m2-grip-content-2 col-md-4 col-sm-4">
+                        <img src="img/coffee-img-1.png">
+                        <h3>Welcome</h3>
+                        <p>“First and foremost I am a chef, whether behind the stove at one of my Northern California restaurants or for the past 15 years in front of the camera on my Food Network cooking shows. Creating new dishes and flavor combinations that bring cooks and our restaurant guests pleasure is my job and I love it.”</p>
                     </div>
+                    <div class="m2-grip-content-1 m2-grip-content-3 col-md-4 col-sm-4">
+                        <div class="m2-grip-content-1-left col-md-10">
+                            <h4>Food</h4>
+                            <p>“Creating new dishes and flavor combinations that bring cooks and our restaurant guests pleasure is my job and I love it.”</p>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
-                <div class="clearfix"></div>
             </div>
         </div>
-    </div>
-</div>    
+    </div>    
 </div>
--- templates/blocks/7002/7002.php
+++ templates/blocks/7002/7002.php
@@ -21,18 +21,10 @@
         <title>Modun 7002</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <?php
-        if (!class_exists('lessc')) {
-            include ('libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/7002.less', 'css/7002.css');
-        ?> 
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/7002.css" rel="stylesheet" type="text/css"/>
-        <script src="js/m1-slide-js.js"></script>
-        <script src="js/modul1-navi-js.js"></script>
+
+       
         <script src="js/jquery-2.1.4.min.js"></script>
         <script src="js/modul-2-js.js"></script>
     </head> 
