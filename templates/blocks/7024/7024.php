b09aa8c396e91a6bb0b633a4e6f486ac469a36f5
Vanthao481994 <vthao481994@gmail.com>
2017-12-22 13:44:36 +0700

    comment
    comment

--- templates/blocks/7024/7024.php
+++ templates/blocks/7024/7024.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <?php
-    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
-    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
-    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-    
-    preg_match_all($pattern_uri, __DIR__, $matches);
-    $url_path = $url_host . $matches[1][0];
-    $url_path = str_replace('\\', '/', $url_path);
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
-    if (!class_exists('lessc')) {
-        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
-        require_once($dir_block.'/libs/lessc.inc.php');
-    }
-    $less = new lessc;
-    $less->compileFile('less/7024.less', 'css/7024.css');
-    
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/7024.less', 'css/7024.css');
 ?>
 <html>
     <head>
@@ -23,8 +22,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js" type="text/javascript"></script>
-        <script src="<?php echo $url_path ?>/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js" ></script>
+        <script src="<?php echo $url_path ?>/js/bootstrap.min.js" ></script>
         <link href="<?php echo $url_path ?>/css/7024.css" rel="stylesheet" type="text/css"/>
     </head> 
 
