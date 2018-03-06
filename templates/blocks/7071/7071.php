a14b78578f8f5ab77aef585c18916e14c6c65742
Vanthao481994 <vthao481994@gmail.com>
2018-03-06 14:29:59 +0700

    Commit
    Commit

--- templates/blocks/7071/7071.php
+++ templates/blocks/7071/7071.php
@@ -22,10 +22,10 @@ $less->compileFile('less/7071.less', 'css/7071.css');
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/7071.css" rel="stylesheet" type="text/css"/>
-        <script src="<?php echo $url_path ?>js/jquery-2.1.4.min.js"></script>
-        <script src="<?php echo $url_path ?>js/m1-slide-js.js"></script>
-        <script src="<?php echo $url_path ?>js/modul1-navi-js.js"></script>
-        <link href="<?php echo $url_path ?>css/font-awesome.min.css" rel="stylesheet">       
+        <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/m1-slide-js.js"></script>
+        <script src="<?php echo $url_path ?>/js/modul1-navi-js.js"></script>
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet">       
     </head> 
     <body> 
         <?php include '../7071/7071-content.php'; ?>
